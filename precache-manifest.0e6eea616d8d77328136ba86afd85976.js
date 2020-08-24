3c1ab1c44f33037d244c5646719d8a299a3099bd
shashankjamdade <jshashank388@gmail.com>
2020-08-24 18:18:31 +0530

    Updates

---
+++ precache-manifest.0e6eea616d8d77328136ba86afd85976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed9009b59695d0a259f47de36576b32",
+    "url": "/react_demo/index.html"
+  },
+  {
+    "revision": "49cc67ef1561517a879d",
+    "url": "/react_demo/static/css/main.17f7033b.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/react_demo/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_demo/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49cc67ef1561517a879d",
+    "url": "/react_demo/static/js/main.91f2a768.chunk.js"
+  },
+  {
+    "revision": "96e44747fe50b084b8a5",
+    "url": "/react_demo/static/js/runtime-main.d1d04a87.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
